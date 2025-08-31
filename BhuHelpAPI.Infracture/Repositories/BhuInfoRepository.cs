92fed6987d4bc99f910033336107deb54e9760d4
RAUSHAN SINGH <raushanaria184@outlook.com>
2025-08-31 06:55:07 +0530

    Worked  on to remove un-necessary project

--- BhuHelpAPI.Infracture/Repositories/BhuInfoRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BhuHelpAPI.Infrastructure.Repositories;
-
-public class BhuInfoRepository : RepositoryBase<BhuInfoEntity>, IBhuInfoRepository
-{
-    public BhuInfoRepository(ApplicationDbContext context) : base(context)
-    {
-    }
-}
